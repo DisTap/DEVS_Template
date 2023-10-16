6bcd046a2416aee7d949951ec99ce94dd5fb28dd
1seyong <30221049@edu.hanbat.ac.kr>
2023-10-16 10:41:26 +0900

    231016 : gen.py copy error fix

--- scripts/gen.py
+++ scripts/gen.py
@@ -5,7 +5,7 @@ import sys
 import shutil
 
 def copy_template_file(directory_path, behavior_name, destination_path=None):
-    template_file = behavior_name
+    template_file = behavior_name +".py"
     source_file_path = os.path.join(directory_path, template_file)
     
     # 파일이 존재하는지 확인합니다.
